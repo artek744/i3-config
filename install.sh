899a6cfe1f21efb15e65c8254db590b06d80013e
Artur Blaszczyk <ablaszczyk44@gmail.com>
2017-01-02 00:02:42 +0100

    Added i3 config files; Updated install.sh script.

--- install.sh
+++ install.sh
@@ -1,20 +1,9 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
-
 
 sudo apt-get update
 sudo apt-get install aptitude
-sudo aptitude install arandr
-sudo aptitude install i3
-sudo aptitude install i3blocks
-sudo aptitude install i3lock
-sudo aptitude install clipit
-sudo aptitude install redshift
-sudo aptitude install feh
-sudo aptitude install vim
-sudo aptitude install qtcreator
-sudo aptitude install google-chrome-stable
-sudo aptitude install lxappearance
-sudo aptitude install arc-theme
-sudo aptitude install rofi
+sudo aptitude install arandr i3 i3blocks i3lock clipit redshift feh vim lxappearance arc-theme rofi
+
+sudo mv src/i3blocks.conf /etc/
+sudo mv src/.config /home/ablaszczyk/
