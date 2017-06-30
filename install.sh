20d8308d2ac7a004e101726b36d35f1b8f3cc0e8
artek744 <ablaszczyk44@gmail.com>
2017-06-30 12:04:37 +0200

    Edit install script

--- install.sh
+++ install.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
 GREEN="\e[32m"
+CYAN="\033[0;96m"
 NC="\e[39m"
-scriptPath=$(cd "$(dirname "$0")" && pwd)
 
-function updateRepositories
-{
-   echo -e $GREEN"Updating repositories..."$NC
-   sudo apt-get update
-}
+scriptPath=$(cd "$(dirname "$0")" && pwd)
 
 function installPrograms
 {
-   echo -e  $GREEN"Installing programs..."$NC
-   sudo apt-get install aptitude
-   sudo aptitude install arandr i3 i3blocks i3lock clipit redshift feh vim lxappearance rofi pcmanfm compton xclip scrot terminator lm-sensors zenity pactl amixer
+	echo -e $CYAN"\nUpdating repositories..."$NC
+	sudo apt-get update
+
+	echo -e  $CYAN"\nInstalling programs..."$NC
+	sudo apt-get  install aptitude arandr i3 i3blocks i3lock clipit redshift feh vim lxappearance rofi pcmanfm compton xclip scrot terminator lm-sensors zenity pactl amixer
 }
 
 function copyMainDirectory
 {
-   echo -e $GREEN"Copying scripts and images..."$NC
+   echo -e $CYAN"\nCopying scripts and images..."$NC
    sudo cp -r src/i3-personal /usr/share/
 }
 
 function deployConfigs
 {
-   echo -e $GREEN"Deploying configs..."$NC
+   echo -e $CYAN"\nDeploying configs..."$NC
    cp -R src/configs/homeDirectory/.  ~/
    $scriptPath/src/configs/i3blocks/deploy-i3blocks.sh
 }
 
-updateRepositories
 installPrograms
 copyMainDirectory
 deployConfigs
